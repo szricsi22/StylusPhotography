c8aa2669f3514ad5778ae8e463deb5e4d8deb1b0
szricsi22 <szabo.ricsi22@gmail.com>
2021-10-05 15:01:49 +0200

    update

--- Pages/models.py
+++ Pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class MyPage(models.Model):
+    title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=200)
+
+    facebook = models.URLField(max_length=200, blank=True)
+    instagram = models.URLField(max_length=200, blank=True)
