2b067025ff72f0792f661f92b46f59f1e1777954
szricsi22 <szabo.ricsi22@gmail.com>
2021-10-06 20:52:22 +0200

    update

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Category(models.Model):
@@ -6,6 +7,25 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Kategóriák"
+
+    def __str__(self):
+        return self.title
+
+
+def image_dir_path(instance, filename):
+    return f"{slugify(instance.category.title)}/{filename}"
+
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    frontpage = models.BooleanField(default=False)
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, blank=True)
+
+    image = models.ImageField(upload_to=image_dir_path)
+    description = models.TextField(max_length=2000)
+    uploaded = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
