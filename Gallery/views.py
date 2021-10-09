4cb10100e57dcf8a73b9c721be01ac248171fe2f
szricsi22 <szabo.ricsi22@gmail.com>
2021-10-09 13:20:55 +0200

    update

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,18 +1,27 @@
-from django.views.generic import TemplateView, DetailView
-
-from utilities.data_gen import create_photo_list
+from django.views.generic import DetailView, ListView
 
 from .models import Category, Photo
 
 
-class GalleryView(TemplateView):
+class GalleryView(ListView):
     template_name = "gallery.html"
+    model = Photo
+    context_object_name = "photos"
 
     def get_context_data(self, **kwargs):
         context = super(GalleryView, self).get_context_data(**kwargs)
-        context["photos"] = Photo.objects.all()
         context["categories"] = Category.objects.all()
         return context
+
+    def get_queryset(self):
+        category_name = self.request.GET.get("category")
+
+        if category_name:
+            photos = Photo.objects.filter(category__title=category_name)
+        else:
+            photos = Photo.objects.all()
+
+        return photos
 
 
 class PhotoDetailView(DetailView):
