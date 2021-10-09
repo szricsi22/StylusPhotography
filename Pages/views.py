f3d283f89dce6041405f5e412a2c7bb0d3cd64dd
szricsi22 <szabo.ricsi22@gmail.com>
2021-10-09 15:37:18 +0200

    update

--- Pages/views.py
+++ Pages/views.py
@@ -30,6 +30,22 @@ class HomeView(TemplateView):
 class ContactView(FormView):
     template_name = "contact.html"
     form_class = ContactForm
+    success_url = "/contact/sent/"
+
+    def form_valid(self, form):
+        name = form.data["name"]
+        email = form.data["email"]
+        message = form.data["message"]
+
+        print("Name:", name),
+        print("Email:", email),
+        print("Message:", message)
+
+        return super(ContactView, self).form_valid(form)
+
+
+class ContactSentView(TemplateView):
+    template_name = "email_sent.html"
 
 
 class AboutView(TemplateView):
