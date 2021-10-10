cdd673544494369a947f732d4ebe7fdcb64cbb3b
szricsi22 <szabo.ricsi22@gmail.com>
2021-10-10 17:57:13 +0200

    update

--- StylusPhotography/settings.py
+++ StylusPhotography/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static_dev']
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
