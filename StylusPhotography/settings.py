c21e671229a42de642a35812517fd18bfe92e048
szricsi22 <szabo.ricsi22@gmail.com>
2021-10-11 08:43:26 +0200

    update

--- StylusPhotography/settings.py
+++ StylusPhotography/settings.py
@@ -26,7 +26,7 @@ load_dotenv(dotenv_path=BASE_DIR / ".env")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['szricsi22.pythonanywhere.com', '127.0.0.1']
 
