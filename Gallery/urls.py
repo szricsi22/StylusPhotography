26b0280e7daea5364afc57f65714c386b1635679
Szabó Ricsi <szabo.ricsi22@gmail.com>
2022-01-21 21:37:37 +0100

    init

--- Gallery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import GalleryView, PhotoDetailView
-
-urlpatterns = [
-    path('', GalleryView.as_view()),
-    path('<str:slug>/', PhotoDetailView.as_view())
-]
--- Pages/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import PageInfo, HomePage, AboutPage
-
-admin.site.register(PageInfo)
-admin.site.register(HomePage)
-admin.site.register(AboutPage)
